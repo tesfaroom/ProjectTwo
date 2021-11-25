155c4d63e2bdb559267ae695d88e46281dda79a3
tesfaroom <tesfaroom@gmail.com>
2021-11-25 00:05:46 -0500

    Update DemoTwoPush.java

--- src/git/two/DemoTwoPush.java
+++ src/git/two/DemoTwoPush.java
@@ -4,6 +4,8 @@ public class DemoTwoPush {
 
 	public static void main(String[] args) {
 		System.out.println(" this is demo two push");
+		
+		System.out.prinln(" This is for pull trial from remot repo to local repo to workspace");
 	}
 
 }
